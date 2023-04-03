9012244c7b790beb3eb39fb5be56176fb706f421
Liam McDevitt <liammcdevitt73@gmail.com>
2023-04-03 15:06:25 -0400

    Adding cuda and updating Pipfile.

--- mlp.py
+++ mlp.py
@@ -151,6 +151,8 @@ class MLP():
         return samples
 
 if __name__ == '__main__':
+    # Device to GPU if available, else CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # Create MLP object
     mlp = MLP('names.txt')
     mlp_data = {
--- name_generator.py
+++ name_generator.py
@@ -114,4 +114,7 @@ class App:
 
 # Main
 if __name__ == '__main__':
+    # Device to GPU if available, else CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     a = App()
